2fa3e74b545a68b199d9c93cf065028001c7e6ce
tomas41730 <43442649+tomas41730@users.noreply.github.com>
2019-05-30 20:58:30 -0400

    Primer avance: Front end

---
+++ mapcontrol/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <v-navigation-drawer
+        fixed
+        v-model="drawer"
+        right
+        app
+      >
+        <v-list dense>
+          <v-list-tile @click="">
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Home</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click="">
+            <v-list-tile-action>
+              <v-icon>contact_mail</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Registrar Conductor</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar color="cyan" dark fixed app>
+        <v-spacer></v-spacer>
+        <v-toolbar-title>Control de Ubicaciones</v-toolbar-title>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout justify-center align-center>
+            <v-flex text-xs-center>
+              <v-tooltip left>
+                <v-btn icon large :href="source" target="_blank" slot="activator">
+                  <v-icon large>code</v-icon>
+                </v-btn>
+                <span>Source</span>
+              </v-tooltip>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+      <v-footer color="cyan" app>
+        <v-spacer></v-spacer>
+        <span class="white--text">&copy; 2019</span>
+      </v-footer>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      drawer: null
+    }
+  }
+}
+</script>
